1cff02b9e718fb84a343866a198e49b1cf4a3ea2
Mohamad Rizal Andy Alkarim <rzalandy@gmail.com>
2021-08-12 11:32:51 +0700

    feat(update): finalize slicing

--- pages/index.vue
+++ pages/index.vue
@@ -35,7 +35,7 @@
                 </div>
               </div>
               <div class="banner-invoice">
-                <img src="/banner.png" class="w-100" alt="" />
+                <img src="/banner-1.png" class="w-100" alt="" />
               </div>
               <b-row class="mt-4">
                 <b-col cols="3">
@@ -72,7 +72,7 @@
                 </div>
               </div>
               <b-row class="mt-4">
-                <b-col cols="3" class="card-product">
+                <b-col cols="3" class="card-product mb-3">
                   <div class="image-product">
                     <img src="/img-1.png" alt="" />
                   </div>
@@ -84,16 +84,304 @@
                       <div class="text-center star-construct">
                         <img src="star.png" class="star" alt="" />
                       </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Kuantitas</span>
+                        <span class="w-50 d-inline-block text-right">30</span>
+                      </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Berat</span>
+                        <span class="w-50 d-inline-block text-right"
+                          >800g/0,8kg</span
+                        >
+                      </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Harga</span>
+                        <span class="w-50 d-inline-block text-right"
+                          >Rp. 105.000</span
+                        >
+                      </div>
                     </div>
-                    <div class="card-detail-quality"></div>
-                    <div class="card-detail-weight"></div>
-                    <div class="card-detail-quantity"></div>
+                  </div> </b-col
+                ><b-col cols="3" class="card-product mb-3">
+                  <div class="image-product">
+                    <img src="/img-1.png" alt="" />
+                  </div>
+                  <div class="card-detail">
+                    <div class="card-detail-title">
+                      <div class="text-center mt-3">
+                        <span class="title-product">Kopi Arabica</span>
+                      </div>
+                      <div class="text-center star-construct">
+                        <img src="star.png" class="star" alt="" />
+                      </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Kuantitas</span>
+                        <span class="w-50 d-inline-block text-right">30</span>
+                      </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Berat</span>
+                        <span class="w-50 d-inline-block text-right"
+                          >800g/0,8kg</span
+                        >
+                      </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Harga</span>
+                        <span class="w-50 d-inline-block text-right"
+                          >Rp. 105.000</span
+                        >
+                      </div>
+                    </div>
+                  </div>
+                </b-col>
+                <b-col cols="3" class="card-product mb-3">
+                  <div class="image-product">
+                    <img src="/img-1.png" alt="" />
+                  </div>
+                  <div class="card-detail">
+                    <div class="card-detail-title">
+                      <div class="text-center mt-3">
+                        <span class="title-product">Kopi Arabica</span>
+                      </div>
+                      <div class="text-center star-construct">
+                        <img src="star.png" class="star" alt="" />
+                      </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Kuantitas</span>
+                        <span class="w-50 d-inline-block text-right">30</span>
+                      </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Berat</span>
+                        <span class="w-50 d-inline-block text-right"
+                          >800g/0,8kg</span
+                        >
+                      </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Harga</span>
+                        <span class="w-50 d-inline-block text-right"
+                          >Rp. 105.000</span
+                        >
+                      </div>
+                    </div>
+                  </div>
+                </b-col>
+                <b-col cols="3" class="card-product mb-3">
+                  <div class="image-product">
+                    <img src="/img-1.png" alt="" />
+                  </div>
+                  <div class="card-detail">
+                    <div class="card-detail-title">
+                      <div class="text-center mt-3">
+                        <span class="title-product">Kopi Arabica</span>
+                      </div>
+                      <div class="text-center star-construct">
+                        <img src="star.png" class="star" alt="" />
+                      </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Kuantitas</span>
+                        <span class="w-50 d-inline-block text-right">30</span>
+                      </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Berat</span>
+                        <span class="w-50 d-inline-block text-right"
+                          >800g/0,8kg</span
+                        >
+                      </div>
+                      <div class="w-100 d-flex card-detail-quality">
+                        <span class="w-50 d-inline-block">Harga</span>
+                        <span class="w-50 d-inline-block text-right"
+                          >Rp. 105.000</span
+                        >
+                      </div>
+                    </div>
                   </div>
                 </b-col>
               </b-row>
+              <b-row class="mt-4 d-flex justify-content-end">
+                <div class="d-flex payment justify-content-end">
+                  <span class="pr-2">Pembayaran melalui BCA</span>
+                  <img src="/bca.png" alt="" />
+                </div>
+              </b-row>
+              <div class="payment-construct mt-4">
+                <div class="d-flex flex-row align-items-center mt-2">
+                  <div class="rounded-title">
+                    <span>Pembayaranmu</span>
+                  </div>
+                  <div class="description-order ml-auto mr-3">
+                    <span>Pembayaran terakhir</span>
+                  </div>
+                  <div class="rounded-purple">
+                    <img
+                      src="/icons-date.png
+"
+                      alt=""
+                    />
+                    19 July 2020 13:05
+                  </div>
+                </div>
+                <b-row class="mt-3">
+                  <b-col cols="3">
+                    <div class="date-construct">
+                      <img
+                        src="/icons-date.png
+"
+                        alt=""
+                      />
+                      <span>19 July 2020 13:05</span>
+                    </div>
+                  </b-col>
+                  <b-col cols="3">
+                    <span class="payment-detail">Bank Transfer</span>
+                  </b-col>
+                  <b-col cols="3">
+                    <span class="payment-detail">SETTLEMENT</span>
+                  </b-col>
+                  <b-col cols="3">
+                    <span class="payment-detail">Rp. 325.000</span>
+                  </b-col>
+                </b-row>
+                <b-row class="mt-3">
+                  <b-col cols="3">
+                    <div class="date-construct">
+                      <img
+                        src="/icons-date.png
+"
+                        alt=""
+                      />
+                      <span>19 July 2020 13:05</span>
+                    </div>
+                  </b-col>
+                  <b-col cols="3">
+                    <span class="payment-detail">Bank Transfer</span>
+                  </b-col>
+                  <b-col cols="3">
+                    <span class="payment-detail">SETTLEMENT</span>
+                  </b-col>
+                  <b-col cols="3">
+                    <span class="payment-detail">Rp. 325.000</span>
+                  </b-col>
+                </b-row>
+                <b-row class="mt-3">
+                  <b-col cols="3">
+                    <div class="date-construct">
+                      <img
+                        src="/icons-date.png
+"
+                        alt=""
+                      />
+                      <span>19 July 2020 13:05</span>
+                    </div>
+                  </b-col>
+                  <b-col cols="3">
+                    <span class="payment-detail">Bank Transfer</span>
+                  </b-col>
+                  <b-col cols="3">
+                    <span class="payment-detail">SETTLEMENT</span>
+                  </b-col>
+                  <b-col cols="3">
+                    <span class="payment-detail">Rp. 325.000</span>
+                  </b-col>
+                </b-row>
+              </div>
             </b-col>
             <b-col cols="4">
-              <div>asndkasndka</div>
+              <div class="map-construct">
+                <div class="invoice d-flex flex-row align-items-center">
+                  <div class="invoice-icon-box">
+                    <div class="rounded-orange">
+                      <img src="/icons-truck.png" alt="" />
+                    </div>
+                  </div>
+                  <div class="invoice-headline pl-3">
+                    <span>Pengiriman</span
+                    ><img class="pl-3" src="/vector-copy.png" alt="" /> <br />
+                    <p>120040042107018</p>
+                  </div>
+                </div>
+                <div class="map">
+                  <img src="map.png" class="w-100" alt="" />
+                </div>
+                <div class="sender-construct">
+                  <div class="sender-detail">
+                    <b-row>
+                      <b-col cols="3" class="sender-ico">
+                        <img src="/sender-icon.jpg" alt="" />
+                      </b-col>
+                      <b-col cols="9">
+                        <div class="sender-title">Pengirim</div>
+                        <div class="sender-name">
+                          Thariq Alfa (085672387343)
+                        </div>
+                        <div class="sender-address">
+                          Jl. Ikan Piranha 105. Kota Malang
+                        </div>
+                        <br />
+                        <div class="sender-title">Penerima</div>
+                        <div class="sender-name">Rizqi O (085672387343)</div>
+                        <div class="sender-address">
+                          Jl. Melati 90 Blok C. Kota Surabaya
+                        </div>
+                      </b-col>
+                    </b-row>
+                    <b-row class="text-center">
+                      <b-col cols="12 mt-4">
+                        <img src="/jne.png" alt="" />
+                      </b-col>
+                    </b-row>
+                  </div>
+                </div>
+              </div>
+              <div class="detail-transaction mt-4">
+                <div class="invoice d-flex flex-row mb-3 align-items-center">
+                  <div class="invoice-icon-box">
+                    <div class="rounded-orange">
+                      <img src="/icons-payment.png" alt="" />
+                    </div>
+                  </div>
+                  <div class="transaction-headline pl-3">
+                    <span>Konfirmasi Total</span
+                    ><img class="pl-3" src="/vector-copy.png" alt="" /> <br />
+                    <p><b>Rp. 325.000</b></p>
+                  </div>
+                </div>
+                <div class="w-100 d-flex detail-price mb-2">
+                  <span class="w-50 d-inline-block">Sub Total</span>
+                  <span class="w-50 d-inline-block text-right"
+                    >Rp. 300.000</span
+                  >
+                </div>
+                <div class="w-100 d-flex detail-price mb-2">
+                  <span class="w-50 d-inline-block">Fee</span>
+                  <span class="w-50 d-inline-block text-right">Rp. 0</span>
+                </div>
+                <div class="w-100 d-flex detail-price mb-2">
+                  <span class="w-50 d-inline-block">Pajak</span>
+                  <span class="w-50 d-inline-block text-right">Rp. 0</span>
+                </div>
+                <div class="w-100 d-flex detail-price mb-2">
+                  <span class="w-50 d-inline-block">Ongkir</span>
+                  <span class="w-50 d-inline-block text-right">Rp. 25.000</span>
+                </div>
+                <div class="w-100 d-flex detail-price mb-5">
+                  <span class="w-50 d-inline-block">Total</span>
+                  <span class="w-50 d-inline-block text-right"
+                    >Rp. 325.000</span
+                  >
+                </div>
+                <div class="w-100 d-flex detail-price mb-5">
+                  <span class="w-50 d-inline-block">Total</span>
+                  <span class="w-50 d-inline-block text-right"
+                    ><b>Rp. 325.000</b></span
+                  >
+                </div>
+                <div class="w-100 text-center border-r-10">Bayar</div>
+                <div class="text-left mt-2 fonts-2">
+                  Membayar dengan CheckoutAja, Anda menyatakan bahwa Anda telah
+                  menyetujui
+                  <span class="note-org"> Syarat dan Ketentuan </span> yang
+                  berlaku.
+                </div>
+              </div>
             </b-col>
           </b-row>
         </b-container>
